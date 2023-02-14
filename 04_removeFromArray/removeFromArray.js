fcff0ba5b751ae7b309a34c301ee86654363b3fb
derekbache <derek.bache@gmail.com>
2023-02-14 15:34:45 -0800

    Provide solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+  for (i = 0; i < toRemove.length; i++) {
+    if (array.indexOf(toRemove[i]) < 0) {
+      continue;
+    } else {
+      array.splice(array.indexOf(toRemove[i]), 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
