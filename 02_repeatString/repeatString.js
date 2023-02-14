8765ed9be30d83f71810d8e62276c5a7250c9e0e
derekbache <derek.bache@gmail.com>
2023-02-14 09:42:20 -0800

    Provide solution for second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeatedString;
+  if (num == 0) {
+    repeatedString = "";
+  } else if (num < 0) {
+    repeatedString = "ERROR";
+  } else {
+    repeatedString = string;
+    for (i = 1; i < num; i++) {
+      repeatedString += string;
+    }
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
