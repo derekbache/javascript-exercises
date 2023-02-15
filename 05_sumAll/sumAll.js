cd4d1ed046ee5ae0aa574a24bc7d13835014e7c7
derekbache <derek.bache@gmail.com>
2023-02-14 16:14:38 -0800

    Provide solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
+  let summedNums = 0;
+  let startInt = firstInt;
+  let endInt = secondInt;
+  if (firstInt < 0 || secondInt < 0) {
+    summedNums = "ERROR";
+  } else if (!(typeof firstInt == "number" && typeof secondInt == "number")) {
+    summedNums = "ERROR";
+  } else {
+    if (firstInt > secondInt) {
+      startInt = secondInt;
+      endInt = firstInt;
+    }
+    for (i = startInt; i < endInt + 1; i++) {
+      summedNums += i;
+    }
+  }
 
+  return summedNums;
 };
 
 // Do not edit below this line
