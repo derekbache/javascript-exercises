410e6f3f153e1309802c45fb064c34e38b62f5e0
derekbache <derek.bache@gmail.com>
2023-03-30 15:08:50 -0700

    Clean up with initial value

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,17 +3,13 @@ const findTheOldest = function (people) {
     if (!("yearOfDeath" in currentPerson)) {
       currentPerson.yearOfDeath = new Date().getFullYear();
     }
-    if (!("yearOfDeath" in oldestPerson)) {
-      oldestPerson.yearOfDeath = new Date().getFullYear();
-    }
-
     let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
     let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
     if (currentAge > oldestAge) {
       oldestPerson = currentPerson;
     }
     return oldestPerson;
-  });
+  }, people[0]);
   return oldest;
 };
 
