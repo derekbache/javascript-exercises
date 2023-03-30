4c967ad25cdd3b0785e8699835ff9037ad58a148
derekbache <derek.bache@gmail.com>
2023-03-30 13:20:09 -0700

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let array = [1, 1];
+  var lastElement = array[array.length - 1];
+  for (i = 2; i <= number; i++) {
+    lastElement = array[array.length - 1];
+    let secondLastElement = array[array.length - 2];
+    array.push(lastElement + secondLastElement);
+  }
+  return lastElement;
 };
 
 // Do not edit below this line
