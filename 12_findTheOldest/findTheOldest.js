ed243de197f135cadbcf2184009c1378ace75806
derekbache <derek.bache@gmail.com>
2023-03-30 15:06:36 -0700

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.reduce((oldestPerson, currentPerson) => {
+    if (!("yearOfDeath" in currentPerson)) {
+      currentPerson.yearOfDeath = new Date().getFullYear();
+    }
+    if (!("yearOfDeath" in oldestPerson)) {
+      oldestPerson.yearOfDeath = new Date().getFullYear();
+    }
 
+    let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    if (currentAge > oldestAge) {
+      oldestPerson = currentPerson;
+    }
+    return oldestPerson;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
